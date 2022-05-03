090631ef66d55861bd97022e0b6dff262bdf260a
KotUW <46974369+KotUW@users.noreply.github.com>
2022-05-03 21:21:02 +0000

    First Implementation of blockchian

--- src/blockchain/blockchain.py
+++ src/blockchain/blockchain.py
@@ -1,19 +1,92 @@
-from block import Block
+from hashlib import sha512
 import time
+import json
+
+class Block:
+    index:int    # The Block number
+    msg:str      # An Array of messages user sent
+    tp:time      # The time of driving the proof (use for conflict resolution)
+    nounce:int   # The nounce. (required for the proof of work Implementation.)
+    prvHash:str  # The hash of the previous hash in the Block Chain
+
+    def __init__(self, index:int, message, timestamp, prvHash, nounce=0):
+        self.id = index
+        self.msg = message
+        self.tp = timestamp
+        self.prvHash = prvHash
+        self.nonce = nonce
+    
+    def computeHash(self):
+        """Computing hash form a message"""
+
+        # Using Json beacuse str one is unrelaible and produces exactly the same output
+        blockStr = json.dumps(self.__dict__, sort_keys=True)
+        return sha256(blockStr.encode()).hexdigest
+
 
 class Blockchain:
-    unconfirmedTrns = []
+    unconfirmedMsgs = []
     chain = []
+    difficulty = 1
 
-    def CreateGenesisBlock(self):
+    def CreateGenesisBlock(self, diff) -> None:
         genBlk = Block(0, [], time.time(), "0")
         genBlk.hash = genBlk.computeHash()
+        
         self.chain.append(genBlk)
+        self.difficulty = diff
     
     @property
-    def lastBlk(self) -> Block:
+    def lastBlck(self) -> Block:
         return self.chain[-1]
 
-    def pow(self, Block):
-    """Proof Of Work used to verify that this server has permision to append."""
+    def pow(self, blck):
+        """Proof Of Work used to verify that this server has permision to append."""
+        blck.nounce = 0
+        computedHash:str = blck.computeHash()
+
+        while not computedHash.startswith('0' * self.difficulty):
+            blck.nonce += 1
+            computedHash = blck.computeHash()
     
+        return computedHash
+
+    def isValidProof(self, Blck, blckHash) -> bool:
+        """Check a Block's Proof before appending it to the chain"""
+        return (blckHash.startswith('0' * self.difficulty) and blckHash == blck.completeHash())
+
+    def addBlck(self, blck, proof) -> bool:
+        """Fucnction to add Unconfirmed Blocks."""
+        prevBlck = self.lastBlck.hash
+        if prevBlck != blck.prevBlck:
+            throw("Not Synced with Chain different last blocks")
+            return False
+
+        if not self.isValidProof(blck, proof):
+            return False
+        
+        blck.hash = proof
+        self.chain.append(blck)
+        return True
+
+    def addNewMsg(self, msg) -> None:
+        self.unconfirmedMsgs.append(msg)
+    
+    def mine(self):
+        """Basic Mining Function that go through any unconfirmed transactions and try to append them to the blockchain"""
+        if not self.unconfirmedMsgs:
+            return False
+        
+        lstBlck = self.lastBlck
+
+        newBlck = Block(index=lstBlck.index + 1, 
+                        message = self.unconfirmedMsgs,
+                        timestamp = time.time(),
+                        prevHash = lstBlck.hash)
+        
+        proof = self.pow(newBlck)
+        self.addBlck(newBlck, proof)
+        self.unconfirmedMsgs = []
+
+        return newBlck.index
+
